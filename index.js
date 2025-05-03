ae6677ffd519082bafff2774ca0d37ad38317757
mozcelik55 <muzi.ozcelik@gmail.com>
2025-05-03 19:08:11 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const { create } = require('venom-bot');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${port}`);
+});
+
 
 let client;
 let qrCodeBase64 = '';
