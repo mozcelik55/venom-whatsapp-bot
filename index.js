8e8986fc5f8c12a7b4566d9311824317a48f2d14
mozcelik55 <muzi.ozcelik@gmail.com>
2025-05-03 18:53:10 +1000

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,9 @@ app.get('/send', async (req, res) => {
   }
 });
 
+ const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
+  console.log(`🚀 Server running on http://0.0.0.0:${port}`);
+});
+
 });
